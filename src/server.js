ad0dba1f80c0d5b6ff0dbc3079d730bc1b9fbcca
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-01-28 11:39:38 +0530

    s

--- src/server.js
+++ src/server.js
@@ -69,6 +69,4 @@ app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 })
 
-app.listen(process.env.PORT || 5000,()=>{
-  console.log("Server Ready on 5000"); //Part #1 Point 5
-});
+app.listen(8000, () => console.log('Server is running on port 8000'));
